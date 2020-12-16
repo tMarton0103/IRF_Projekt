4a640cdab865a4603cdb0347833fb499b7cd4474
Tamás Márton <mtomi0103@gmail.com>
2020-12-16 12:26:42 +0100

    Szelvenyek class

---
+++ Szerencsejatek/Szelvenyek.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szerencsejatek
+{
+    public class Szelvenyek
+    {
+        public int Szelveny_Id { get; set; }
+        public int t01 { get; set; }
+        public int t02 { get; set; }
+        public int t03 { get; set; }
+        public int t04 { get; set; }
+        public int t05 { get; set; }
+    }
+}
